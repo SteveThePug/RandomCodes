96f8efd25acbce1e3539d53daa11173a2b8f65f9
SteveThePug <45966892+SteveThePug@users.noreply.github.com>
2020-12-03 17:27:01 +0000

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,15 @@
+local component = require("component")
+local sides = require("sides")
+local rs = component.redstone
+local inventory = component.inventory_controller
+
+local slot = 1
+local amount = inventory.getStackInInternalSlot(slot)
+if amount then
+    print(item.name)
+    print(item.size)
+    print(item.damage)
+
+else
+    print("Slot empty")
+end
